43803e1d664a83523a24b84ce67f3f8f80281885
Logan-Williams <maksimlebedev200.ru@gmail.com>
2024-05-06 17:46:37 +0400

    код для задангия 1

---
+++ program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+ class Program
+{
+    private static void Main()
+    {
+        int M = 1; // начальное значение M
+        int N = 10; // конечное значение N
+        
+        PrintNumbers(M, N);
+    }
+
+    private static void PrintNumbers(int current, int end)
+    {
+        if (current <= end)
+        {
+            Console.WriteLine(current);
+            PrintNumbers(current + 1, end);
+        }
+    }
+}
